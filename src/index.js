dce5df3bdaaf25c93375e1806077416dd1cf7300
Christian Sinvry <sinvry.christian@gmail.com>
2018-02-02 00:25:54 +0100

    localstorage and new component

--- src/index.js
+++ src/index.js
@@ -3,18 +3,34 @@ import { render } from 'react-dom';
 //CSS
 //JS
 import { sampleText } from './sampleText';
+//marked
+import marked from 'marked';
 
 
 class App extends React.Component {
+
 
     state = {
         text: sampleText
     };
 
+    componentWillMount() {
+        const localStorageText = localStorage.getItem('text');
+    };
+
+    componentWillUpdate(nextProps, nextState) {
+        localStorage.setItem('text', nextState.text);
+    };
+
     editText = (event => {
         const text = event.target.value;
         this.setState({ text });
-    })
+    });
+
+    renderText = (text) => {
+       const renderText = marked(text, {sanitize: true});
+       return { __html: renderText }; 
+    };
 
     render() {
         return (
@@ -32,7 +48,9 @@ class App extends React.Component {
                 </div>
 
                 <div className="col-sm-6">
-                    {this.state.text}
+                    <div dangerouslySetInnerHTML={this.renderText(this.state.text)}>
+
+                    </div>
                 </div>
 
                 </div>
