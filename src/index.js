7e150eecc0f0303f59f48a84b9977ee04650e9ca
Christian Sinvry <sinvry.christian@gmail.com>
2018-02-01 23:51:11 +0100

    create on state

--- src/index.js
+++ src/index.js
@@ -6,19 +6,24 @@ import { sampleText } from './sampleText';
 
 
 class App extends React.Component {
+
+    state = {
+        text: sampleText
+    };
+
     render() {
         return (
             <div className="container">
                 <div className="row">
 
                 <div className="col-sm-6">
-                <textarea value={sampleText} rows="35" className="form-control">
+                <textarea value={this.state.text} rows="35" className="form-control">
 
                 </textarea>
                 </div>
 
                 <div className="col-sm-6">
-                    {sampleText}
+                    {this.state.text}
                 </div>
 
                 </div>
